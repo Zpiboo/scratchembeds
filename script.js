6fe054c97c2452d56e10eb1da0b5409fa77dfaab
Zpiboo <lolmdrrvjv@gmail.com>
2022-01-22 19:08:32 +0100

    Updated Javascript (Replaced xhr by fetch, test #1)

--- script.js
+++ script.js
@@ -2,13 +2,9 @@ const urlParameters = new URLSearchParams(location.search);
 var projectID = urlParameters.get("id");
 const apiEndpoint = "https://api.scratch.mit.edu";
 
-var projectRequest = new XMLHttpRequest();
-projectRequest.onreadystatechange = function () {
-  if (this.readyState == 4) {
-    document.body.innerText = projectRequest.responseText;
-  }
-};
-
-projectRequest.open("GET", `${apiEndpoint}/projects/${projectID}`);
-projectRequest.setRequestHeader("Access-Control-Allow-Origin", "https://zpiboo.github.io/");
-projectRequest.send();
+fetch(`${apiEndpoint}/projects/${projectID}`, {mode:"cors"})
+  .then(function () {
+    if (this.readyState == 4) {
+      document.body.innerText = projectRequest.responseText;
+    }
+  });
